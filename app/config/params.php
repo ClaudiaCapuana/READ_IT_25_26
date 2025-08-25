24dc69695883608aca2ae19cf119aa4c2639deb6
ClaudiaC18 <claudia.capuana@gmail.com>
2025-08-25 15:25:43 +0200

    Mise en place de la structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,9 @@
+<?php
+// init des zones dynamiques
+$content = '';
+$title = '';
+
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'readit_2020');
+define('DB_USER', 'root');
+define('DB_PWD', 'root');
