711f98cf3e429c216b081019707471d2f3cbe4fc
ClaudiaC18 <claudia.capuana@gmail.com>
2025-09-02 11:11:39 +0200

    categories Model

--- app/views/templates/partials/_head.php
+++ app/views/templates/partials/_head.php
@@ -1,6 +1,7 @@
 <title>Readit - <?php echo $title; ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- ! risque d'oublier -->
     <base href="<?php echo PUBLIC_BASE_URL; ?>">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 
